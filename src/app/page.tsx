5428fcdaadd204bfa2b997606caea16993e76def
dishu009 <147999734+dishu009@users.noreply.github.com>
2024-06-11 06:54:52 -0700

    add
    Added more Comps FOr A good Porto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
 import Image from "next/image";
+import Hero from "./Hero/page"
+import AboutMe from "./AboutMe/page"
+import Projects from "./Projects/page"
+import Contact from "./Contact/page"
 
 export default function Home() {
   return (
@@ -6,6 +10,10 @@ export default function Home() {
       <div className="sticky top-0 z-20">
         <h1 className="text-2xl font-semibold">Shubham Maurya</h1>
       </div>
+      <Hero />
+      <AboutMe />
+      <Projects/>
+      <Contact/>
     </main>
   );
 }
