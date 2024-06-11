ba84a41fad560e0654f1561dac44cd76e5a21ec2
dishu009 <147999734+dishu009@users.noreply.github.com>
2024-06-11 16:28:25 +0530

    Accternity, Shandcn
    Accternity, ShandcnAccternity, ShandcnAccternity, ShandcnAccternity, ShandcnAccternity, Shandcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8">
-      <h1 className="text-2xl font-semibold">Shubham Maurya</h1>
-    
+      <div className="sticky top-0 z-20">
+        <h1 className="text-2xl font-semibold">Shubham Maurya</h1>
+      </div>
     </main>
   );
 }
